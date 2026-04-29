72d28218edbb9b008e5c80c17496bae78be74662
Frances2345 <francesco.moreno@tecsup.edu.pe>
2026-04-29 12:14:07 -0500

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //public MyQueue<string> BankQueue = new();
+
+    //public float speed = 0;
+    //public float MaxSpeed = 100;
+    
+    /*public static GameManager instance;
+    public PriorityQueue<Entity> priorityQueue =
+        new(  (a,b) => a.stats.speed < b.stats.speed  );
+
+    public List<Entity> entities = new();
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void AutoAdd(Entity entities)
+    {
+        entities.Add(entity);
+    }
+
+    public void EnquequeAll()
+    {
+        foreach (ValidatorSeverity entity in entities)
+        {
+            Enqueque(entity);
+        }
+    }*/
+
+    /*void Start()
+    {
+        
+    }
+    [Button]
+    public void Enqueue(string name)
+    {
+        BankQueue.Enqueue(name);
+    }
+    [Button]
+
+    public void Dequeue()
+    {
+       Debug.Log("Pase a ser atendido : " +  BankQueue.Dequeue());
+    }
+    [Button]
+    public void Peek()
+    {
+        Debug.Log("El siguiente en ser atendido sera ... " + BankQueue.Peek());
+    }
+
+    [Button]
+    public void Clear()
+    {
+        BankQueue.Clear();  
+    }
+
+    [Button]
+    public void Count()
+    {
+        Debug.Log( BankQueue.Count);
+    }*/
+
+
+
+}
---
+++ Assets/Scripts/Queues/Entity.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Entity", menuName = "Scriptable Objects/Entity")]
+public class Entity : ScriptableObject
+{
+    public float speed;//->mayor velocidad
+    public float id;//->menor id
+}
